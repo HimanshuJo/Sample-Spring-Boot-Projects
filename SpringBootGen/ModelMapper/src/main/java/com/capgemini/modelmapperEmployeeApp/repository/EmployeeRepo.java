0a3adc7e30b22d7022748776b21f24c3b982f8ee
CORP\himjoshi <himanshu.a.joshi@capgemini.com>
2023-10-04 14:10:46 +0200

    Added projects

--- SpringBootGen/ModelMapper/src/main/java/com/capgemini/modelmapperEmployeeApp/repository/EmployeeRepo.java
+++
@@ -1,8 +0,0 @@
-package com.capgemini.modelmapperEmployeeApp.repository;
-
-import com.capgemini.modelmapperEmployeeApp.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
-
-}
